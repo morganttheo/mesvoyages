b36293b6065d71abd77d8c348df2332f63b2d994
morganttheo <oehttnagrom@gmail.com>
2023-10-25 12:23:17 +0200

    Changement couleur titre

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -50,7 +50,7 @@ class ContactController extends AbstractController {
     public function sendEmail(MailerInterface $mailer, Contact $contact){
         $email = (new Email())
             ->from($contact->getEmail())
-            ->to('contact@mesvoyages.com')
+            ->to('flopipoop@gmail.com')
             ->subject('Message du site de voyages')
             ->html($this->renderView(
                     'pages/_email.html.twig',[
